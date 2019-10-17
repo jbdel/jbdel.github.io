ed10fc6460c5efae34aa350b526a78db18f68dcb
jbdel <jeanbenoit.delbrouck@gmail.com>
2019-10-17 16:32:08 +0200

    Update plotting.py

--- teaching/nn/ressources/rl/lib/plotting.py
+++ teaching/nn/ressources/rl/lib/plotting.py
@@ -62,7 +62,7 @@ def plot_value_function(V, title="Value Function"):
 
 def plot_episode_stats(stats, smoothing_window=10, noshow=False):
     # Plot the episode length over time
-    fig1 = plt.figure(figsize=(10,5))
+    fig1 = plt.figure(figsize=(5,3))
     plt.plot(stats.episode_lengths)
     plt.xlabel("Episode")
     plt.ylabel("Episode Length")
@@ -73,7 +73,7 @@ def plot_episode_stats(stats, smoothing_window=10, noshow=False):
         plt.show(fig1)
 
     # Plot the episode reward over time
-    fig2 = plt.figure(figsize=(10,5))
+    fig2 = plt.figure(figsize=(5,3))
     rewards_smoothed = pd.Series(stats.episode_rewards).rolling(smoothing_window, min_periods=smoothing_window).mean()
     plt.plot(rewards_smoothed)
     plt.xlabel("Episode")
@@ -85,7 +85,7 @@ def plot_episode_stats(stats, smoothing_window=10, noshow=False):
         plt.show(fig2)
 
     # Plot time steps and episode number
-    fig3 = plt.figure(figsize=(10,5))
+    fig3 = plt.figure(figsize=(5,3))
     plt.plot(np.cumsum(stats.episode_lengths), np.arange(len(stats.episode_lengths)))
     plt.xlabel("Time Steps")
     plt.ylabel("Episode")
