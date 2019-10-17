a6e08bd2399c0be2316212fb1b03ad39467acf45
jbdel <jeanbenoit.delbrouck@gmail.com>
2019-10-17 16:24:58 +0200

    Update cliff_walking.py

--- teaching/nn/ressources/rl/lib/envs/cliff_walking.py
+++ teaching/nn/ressources/rl/lib/envs/cliff_walking.py
@@ -19,15 +19,18 @@ class CliffWalkingEnv(discrete.DiscreteEnv):
         coord[1] = max(coord[1], 0)
         return coord
 
-    def _calculate_transition_prob(self, transition_prob, current, delta):
+    def _calculate_transition_prob(self, current, delta, prob=1.0, wind=False):
         new_position = np.array(current) + np.array(delta)
         new_position = self._limit_coordinates(new_position).astype(int)
         new_state = np.ravel_multi_index(tuple(new_position), self.shape)
         reward = -100.0 if self._cliff[tuple(new_position)] else -1.0
         is_done = self._cliff[tuple(new_position)] or (tuple(new_position) == (3,11))
-        return [(transition_prob, new_state, reward, is_done)]
+        if not wind:
+            return [(prob, new_state, reward, is_done)]
+        else:
+            return [(0.8, new_state, reward, is_done)] + self._calculate_transition_prob(current, [1, 0], prob=0.2)
 
-    def __init__(self, transition_prob=1.0):
+    def __init__(self, wind=False):
         self.shape = (4, 12)
 
         nS = np.prod(self.shape)
@@ -42,10 +45,10 @@ class CliffWalkingEnv(discrete.DiscreteEnv):
         for s in range(nS):
             position = np.unravel_index(s, self.shape)
             P[s] = { a : [] for a in range(nA) }
-            P[s][UP] = self._calculate_transition_prob(transition_prob, position, [-1, 0])
-            P[s][RIGHT] = self._calculate_transition_prob(transition_prob, position, [0, 1])
-            P[s][DOWN] = self._calculate_transition_prob(transition_prob, position, [1, 0])
-            P[s][LEFT] = self._calculate_transition_prob(transition_prob, position, [0, -1])
+            P[s][UP] = self._calculate_transition_prob(position, [-1, 0])
+            P[s][RIGHT] = self._calculate_transition_prob(position, [0, 1], wind=wind)
+            P[s][DOWN] = self._calculate_transition_prob(position, [1, 0])
+            P[s][LEFT] = self._calculate_transition_prob(position, [0, -1])
 
         # We always start in state (3, 0)
         isd = np.zeros(nS)
@@ -82,3 +85,4 @@ class CliffWalkingEnv(discrete.DiscreteEnv):
 
             outfile.write(output)
         outfile.write("\n")
+
